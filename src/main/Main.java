a3e43b7c33fc640796b31bdfea4b28b8c8d0e416
k899e <k8.99e9@gmail.com>
2021-02-10 10:48:17 -0500

    fix gui size

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1400, 800));
         primaryStage.show();
     }
     public static void main(String[] args) {
