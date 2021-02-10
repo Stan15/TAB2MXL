4747b0f2def882a6972f604a88118ed6de2ec12a
k899e <k8.99e9@gmail.com>
2021-02-10 02:41:41 -0500

    Gui should generate file from parse output given input within text area

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,4 +1,48 @@
 package main;
 
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import parser.Parser;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Logger;
+
 public class Controller {
+    @FXML private AnchorPane anchorPane;
+
+    @FXML private TextArea inputField;
+
+    @FXML
+    private void convertButtonHandle() {
+        Parser p = new Parser(inputField.getText());
+        String output = p.parse();
+
+        FileChooser fc = new FileChooser();
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML files (*.xml)", "*.xml");
+        fc.getExtensionFilters().add(extFilter);
+
+
+        File file = fc.showSaveDialog( (Stage) anchorPane.getScene().getWindow() );
+
+        if (file != null) {
+            saveToFile(output, file);
+        }
+    }
+
+    private void saveToFile(String content, File file) {
+        try {
+            PrintWriter writer;
+            writer = new PrintWriter(file);
+            writer.println(content);
+            writer.close();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -15,6 +15,6 @@ public class Main extends Application {
         primaryStage.show();
     }
     public static void main(String[] args) {
-        launch(gui.GUI.class, args);
+        launch(args);
     }
 }
