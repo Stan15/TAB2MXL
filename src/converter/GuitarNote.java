e9e9339628f90446da7d1e02acc56728640e6283
Adil Hashmi <adilhashmi12345@gmail.com>
2021-02-09 19:52:42 -0500

    Implemented measureLine, Created new note object with its distances. Implemented note.from class to get note instances.

--- src/converter/GuitarNote.java
+++ src/converter/GuitarNote.java
@@ -1,7 +1,7 @@
 package converter;
 
 public class GuitarNote extends Note{
-    public GuitarNote(int stringNumber, int fret, int duration, int distanceToMeasureStart) {
-        super(stringNumber, fret, duration, distanceToMeasureStart);
+    public GuitarNote(String lineName, int fret, int duration, int distanceToMeasureStart) {
+        super(lineName, fret, duration, distanceToMeasureStart);
     }
 }
