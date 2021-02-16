3c58f65597e57242478614906da8e60618cd0b05
Stanley Ihesiulo <stanihe1901@gmail.com>
2021-02-15 22:48:46 -0500

    made Main.java run the java fxml gui, instead of the previous GUI

--- src/main/Main.java
+++ src/main/Main.java
@@ -16,6 +16,6 @@ public class Main extends Application {
         primaryStage.show();
     }
     public static void main(String[] args) {
-        launch(gui.GUI.class, args);
+        launch(args);
     }
 }
